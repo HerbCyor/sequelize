d1384fdc42b2315209e828db6c7159760572c9cd
herbert <herbert.cardozo@gmail.com>
2024-04-10 20:18:37 -0300

    atualizado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,20 @@
 const { Router } = require("express")
+const Aluno = require("../models/aluno")
 
 const routes = new Router()
 
 routes.get("/bem_vindo", (req, res) => {
     res.send('Bem-Vindo')
+})
+
+
+routes.post("/alunos", async (req, res) => {
+    const aluno = await Aluno.create({
+        nome: "Herbert Martins Cardozo",
+        data_nascimento: "1988-09-10",
+        celular: "123456789"
+    })
+    res.json({ aluno })
 })
 
 module.exports = routes
